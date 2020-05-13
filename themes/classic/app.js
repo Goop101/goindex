e82d104a49ba93086eea8b557ccf0131c2b51981
r4nchy <42188371+alx-xlx@users.noreply.github.com>
2020-05-13 21:36:11 +0530

    themes/classic/app:

--- themes/classic/app.js
+++ themes/classic/app.js
@@ -1,19 +1,3 @@
-// document.write('<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/mdui@0.4.3/dist/css/mdui.min.css">');
-// document.write('<script src="//cdn.jsdelivr.net/npm/markdown-it@10.0.0/dist/markdown-it.min.js"></script>');
-document.write('<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/mdui@0.4.3/dist/css/mdui.min.css">');
-// markdown支持
-document.write('<script src="//cdn.jsdelivr.net/npm/markdown-it@10.0.0/dist/markdown-it.min.js"></script>');
-document.write(`<style>.mdui-appbar .mdui-toolbar{height:56px;font-size:1pc}
-.mdui-toolbar>*{padding:0 6px;margin:0 2px}
-.mdui-toolbar>i{opacity:.5}.mdui-toolbar>.mdui-typo-headline{padding:0 1pc 0 0}
-.mdui-toolbar>i{padding:0}.mdui-toolbar>a:hover,a.active,a.mdui-typo-headline{opacity:1}
-.mdui-container{max-width:980px}.mdui-list-item{transition:none}.mdui-list>
-.th{background-color:initial}.mdui-list-item>a{width:100%;line-height:3pc}
-.mdui-list-item{margin:2px 0;padding:0}
-.mdui-toolbar>a:last-child{opacity:1}@media screen and (max-width:980px){.mdui-list-item .mdui-text-right{display:none}
-.mdui-container{width:100%!important;margin:0}.mdui-toolbar>.mdui-typo-headline,.mdui-toolbar>a:last-child,
-.mdui-toolbar>i:first-child{display:block}}</style>`);
-
 document.write(`
 <style>
 *{box-sizing:border-box}
@@ -118,7 +102,7 @@ function list(path){
 	
     var password = localStorage.getItem('password'+path);
 	
-	$('#list').html(`<div class="mdui-progress"><div class="mdui-progress-indeterminate"></div></div>`);
+	// $('#list').html(`<div class="mdui-progress"><div class="mdui-progress-indeterminate"></div></div>`);
 	// $('#list').html(html);
     $.post(path,'{"password":"'+password+'"}', function(data,status){
         var obj = jQuery.parseJSON(data);
@@ -340,7 +324,7 @@ function list_files(path,files){
         }
     }
     $('#table').append(html);
-	$('#list').html(html);
+	// $('#list').html(html);
 }
 
 
